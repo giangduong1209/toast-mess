0503ea61add539a80d91a73d7815bd3a343a32ed
giangduong1209 <giangduong120900@gmail.com>
2021-07-29 18:17:29 +0700

    setTimeOut for toast

--- script.js
+++ script.js
@@ -4,22 +4,49 @@ function toast({
     type = '',
     duration = 3000
 })  {
+
+
     const main = document.getElementById("toast");
     if(main){
-        const toast = document.createElement("div")
-        toast.classList.add("toast", `toast-${type}`)
+        const toast = document.createElement("div");
+
+        const autoRemove =  setTimeout(() => {
+            main.removeChild(toast);
+        }, duration + 1000);
+
+        toast.onclick = function(e){
+           if(e.target.closest(".toast__close")){
+               main.removeChild(toast);
+               clearTimeout(autoRemove);
+           }
+        }
+
+        const icons ={
+            success: "fas fa-check-circle",
+            info: "fas fa-info-circle",
+            danger: "fas fa-exclamation-circle"
+        }
+
+        
+
+        const icon = icons[type];
+        const delay = (duration / 1000).toFixed(2);
+        toast.classList.add("toast", `toast-${type}`);
+        toast.style.animation = `fadeIn ease .3s, fadeOut linear 1s ${delay}s forwards`;
         toast.innerHTML = `
             <div class="toast__icon">
-                <i class="fas fa-check"></i>
+                <i class="${icon}"></i>
             </div>
             <div class="toast__body">
-                <h3 class="toast__title">Thành công</h3>
-                <p class="toast__msg">Bạn đã đăng ký tài khoản thành công.</p>
+                <h3 class="toast__title">${title}</h3>
+                <p class="toast__msg">${message}.</p>
             </div>
             <div class="toast__close">
                 <i class="fas fa-times"></i>
             </div>
         `;
         main.appendChild(toast);
+
+
     }
 }
